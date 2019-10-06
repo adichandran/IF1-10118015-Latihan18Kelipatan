c1170202662d4709403d5324884a72b2a7196ea1
adichandran <55467774+adichandran@users.noreply.github.com>
2019-10-06 15:55:39 +0700

    Add files via upload

---
+++ src/if1/pkg10118015/latihan18kelipatan/IF110118015Latihan18kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author 
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program ini berisi program kelipatan 3,5
+ */
+public class IF110118015Latihan18kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i <= 35; i+=3.5) 
+            System.out.println(i);
+        System.out.println("(Developed by : Adi Chandra Nugraha)");
+    }
+    
+}
